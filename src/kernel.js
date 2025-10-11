5871a9047185b9301a13b57efb4592927fa150d3
KukakoRPG <xefaos@gmail.com>
2025-10-11 10:43:24 +0300

    Update kernel.js

--- src/kernel.js
+++ src/kernel.js
@@ -303,19 +303,20 @@ kernel.init = function init( cmdLineContainer, outputContainer ) {
         cmdLine_ = document.querySelector( cmdLineContainer );
         output_ = document.querySelector( outputContainer );
 
-        $.when(
-            $.get( "config/software.json", ( softwareData ) => {
-                softwareInfo = softwareData;
-                kernel.connectToServer( defaultServerAddress );
-            } )
-        )
-            .done( () => {
-                resolve( true );
-            } )
-            .fail( ( err, msg, details ) => {
-                console.error( "[init] Failure:", err, msg, details );
-                reject( new JsonFetchParseError( msg ) );
-            } );
+        $.get( "config/software.json", ( softwareData ) => {
+            softwareInfo = softwareData;
+            kernel.connectToServer( defaultServerAddress )
+                .then( () => {
+                    resolve( true );
+                } )
+                .catch( ( err ) => {
+                    console.error( "[init] Failure during connectToServer:", err );
+                    reject( err );
+                } );
+        } ).fail( ( err, msg, details ) => {
+            console.error( "[init] Failure loading software.json:", err, msg, details );
+            reject( new JsonFetchParseError( msg ) );
+        } );
     } );
 };
 
